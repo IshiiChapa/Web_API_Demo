c9b8fd715ac8ea90a2576682824c4cb0b447a7b8
lakinsha <lakinsha41@gmail.com>
2021-05-20 03:14:39 +0530

    commited by lakinsha

--- shoppingcart_backend/routes/category.js
+++ shoppingcart_backend/routes/category.js
@@ -6,5 +6,15 @@ const router = express.Router();
 
 router.post('/category/create', addCategory);
 router.get('/category/getcategory', getCategories);
+
+router.put('/category/:categoryId/:userId', requireSignin, isAdmin, isAdmin, update);
+
+router.delete('/category/:categoryId/:userId', requireSignin, isAdmin, isAdmin, remove);
+
+router.get('/categories', list);
+
+router.param('categoryId', categoryById);
+
+router.param('userId', userById);
  
 module.exports = router;
