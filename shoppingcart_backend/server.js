db9c53effcb290dc26f3fd8f32f29acba9de7418
IshiiChapa <67183182+IshiiChapa@users.noreply.github.com>
2021-05-19 18:55:19 +0530

    added category api related files post/get

--- shoppingcart_backend/server.js
+++ shoppingcart_backend/server.js
@@ -6,6 +6,7 @@ const signup = require('./routes/signup')
 const auth = require('./routes/auth')
 const productlist = require('./routes/product')
 const cors = require('cors')
+const categoryRoutes = require('./routes/category');
 
 env.config()
 mongoose.connect(process.env.DATABASE_CONNECTION_URL, () => console.log("Database Connected"))
@@ -15,4 +16,8 @@ app.use(cors())
 app.use('/api/signup', signup)
 app.use('/api/auth', auth)
 app.use('/api/product', productlist)
+app.use('/api',categoryRoutes);
+
+
+
 app.listen(2000, ()=> console.log("Server is up and Running...."))
