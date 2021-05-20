549ca232a54f1bd931e075a3e7637a681f7c60af
lakinsha <lakinsha41@gmail.com>
2021-05-20 07:13:49 +0530

    Create AdminDashboard.js

---
+++ shoppingcart_frontend/src/views/Admin/AdminDashboard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const AdminDashboard = () => {
+
+    const showHeader = () => (
+        <div className='bg-dark text-white py-4'>
+            <div className='container'>
+            <div className='row'>
+                <div className='col-md-6'>
+                <h1> <i className='fas fa-home'> Dashboard</i>
+                </h1>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    )
+    return (
+        
+        // <section>
+        //     {(showHeader)}
+        // </section>
+        <h1>test</h1>
+
+    )
+};
+
+export default AdminDashboard;
